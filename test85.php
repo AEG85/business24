27057c5381b751e55befbc60ad77b3c36c676750
PC Evgen <ahtirsk@gmail.com>
2020-05-25 23:36:45 +0600

    First commit

---
+++ test85.php
@@ -0,0 +1,19 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetTitle("тест85");
+?>Text here....
+<?
+echo '<pre>';
+print_r($_SERVER);
+echo '</pre>';
+Bitrix\Main\Loader::includeModule('iblock');
+
+echo Bitrix\Iblock\IblockTable::getTableName();
+echo '<pre>'; print_r($arResult); echo '</pre>';
+echo '<pre>'; print_r($arResult); echo '</pre>';
+echo '<pre>'; print_r($arResult); echo '</pre>';
+echo '<pre>'; print_r($arResult); echo '</pre>';
+
+?>
+
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
